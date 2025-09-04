b88b0fb8e6fbcf721e4d6143c0a32bb630c4fd88
sdeossag <sdeossag@eafit.edu.co>
2025-09-04 17:04:06 -0500

    Cambios

--- PetShopProject/urls.py
+++ PetShopProject/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tienda.urls')),
+    path('productos/', include('productos.urls'))
 
 ]
 if settings.DEBUG:  
