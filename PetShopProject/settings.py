76f109bc1e1a6fce4ea0f40ad17f54bc57121055
Antonia Muñoz <mamunozj@eafit.edu.co>
2025-11-04 17:40:43 -0500

    AWS

--- PetShopProject/settings.py
+++ PetShopProject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "insecure-default-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "54.221.172.76"]
 
 
 
